d45844cc6b15b20feff59ec0dd2dbf7dfdfa5df9
dgrimaliuc <116957079+dgrimaliuc@users.noreply.github.com>
2023-06-28 23:31:46 +0300

    Test Updated (#1)
    Fixed Test Runner

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -5,8 +5,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/",
-        glue = {"src/test/java/actionLibraries/"},
-        tags = {"@Smoke and not @SmokeWrapper"},
+        glue = {"actionLibraries"},
+        tags = {"@Smoke"},
         plugin = {"pretty", "html:target/cucumber", "json:target/cucumber-html-reports/cucumber.json"})
 
 public class TestRunner {
--- src/test/resources/denis_grimaliuc/First Steps in Cucumber tes.feature
+++ src/test/resources/denis_grimaliuc/First Steps in Cucumber tes.feature
@@ -1,5 +1,7 @@
-@Smoke
 Feature: First Steps in Cucumber
 
+  @Smoke
   Scenario: Addition of two numbers
-    Given validate password "qwe1"
+    Given I have two numbers "5" and "6"
+    When I add the numbers
+    Then the result should be "11"
