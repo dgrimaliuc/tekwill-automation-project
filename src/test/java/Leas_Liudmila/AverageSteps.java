65736d9c8955d5f1bfca387e789479cbf476df4f
dinom <dinomc222@mail.ru>
2023-06-30 12:52:52 +0300

    fix unknown imports

--- src/test/java/Leas_Liudmila/AverageSteps.java
+++ src/test/java/Leas_Liudmila/AverageSteps.java
@@ -1,8 +1,8 @@
 package Leas_Liudmila;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.apache.log4j.Logger;
 import org.hamcrest.Matchers;
 import static org.hamcrest.MatcherAssert.assertThat;
