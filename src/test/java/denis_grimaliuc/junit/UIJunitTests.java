9e23d8f619fe25af6df774442a3d8ad88e5a9a77
Denis Grimaliuc <denis.grimaliuc@crunchyroll.com>
2023-07-19 19:23:21 +0300

    Example of UI Junit test

---
+++ src/test/java/denis_grimaliuc/junit/UIJunitTests.java
@@ -0,0 +1,74 @@
+package denis_grimaliuc.junit;
+
+import denis_grimaliuc.AdoptPage;
+import denis_grimaliuc.UIStepDefinition;
+import denis_grimaliuc.junit.actions.BaseActions;
+import helpers.Helpers;
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import static denis_grimaliuc.AdoptPage.FIRST_ROW_IN_TABLE;
+import static helpers.Helpers.stepResults;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class UIJunitTests {
+
+    WebDriver driver = null;
+    BaseActions actions = null;
+    WebDriverWait wait = null;
+    Logger log = Logger.getLogger(UIStepDefinition.class);
+    AdoptPage page = null;
+
+    @BeforeEach
+    public void before() {
+        var pathToChrome = "src/main/resources/chromedriver_mac";
+        System.setProperty("webdriver.chrome.driver", pathToChrome);
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(5, TimeUnit.SECONDS);
+        actions = new BaseActions(driver);
+        wait = new WebDriverWait(driver, 5);
+        stepResults = new ArrayList<>();
+        page = new AdoptPage(driver, wait);
+        //        Runtime.getRuntime().addShutdownHook(new Thread(() -> driver.quit()));
+    }
+
+    @AfterEach
+    public void after() {
+        driver.quit();
+    }
+
+    @Test
+    @DisplayName("Added pet in different location is not reflected in current one test")
+    public void test1() {
+        actions.openRandomLocation();
+        actions.openRandomLocation();
+        actions.addAPetToCurrentLocation();
+
+        driver.navigate().back();
+        Helpers.waitInSeconds(1);
+        assertThat(page.pets.size(), equalTo(1));
+        wait.until(ExpectedConditions.textToBe(FIRST_ROW_IN_TABLE, "No rows. Try reset filters"));
+    }
+
+    @Test
+    @DisplayName("Window can be opened in new tab test")
+    public void test2() {
+        actions.openRandomLocation();
+        page.openNewTabBtn.click();
+        actions.verifyNewTabOpened();
+    }
+}
---
+++ src/test/java/denis_grimaliuc/junit/actions/BaseActions.java
@@ -0,0 +1,54 @@
+package denis_grimaliuc.junit.actions;
+
+import denis_grimaliuc.AdoptPage;
+import helpers.Helpers;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.log4j.Logger;
+import org.hamcrest.Matchers;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static denis_grimaliuc.AdoptPage.FIRST_ROW_IN_TABLE;
+import static helpers.Helpers.addQuotes;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class BaseActions {
+
+    static Logger log = Logger.getLogger(BaseActions.class);
+    private final WebDriver driver;
+    private final AdoptPage page;
+    private final WebDriverWait wait;
+
+    public BaseActions(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 5);
+        page = new AdoptPage(driver, wait);
+    }
+
+    public void verifyNewTabOpened() {
+        Object[] tabs = driver.getWindowHandles().toArray();
+        assertThat(tabs.length, Matchers.equalTo(2));
+        String currentTabId = driver.getWindowHandle();
+        assertThat(currentTabId, equalTo(tabs[0]));
+    }
+
+    public String openRandomLocation() {
+        String randomLocation = RandomStringUtils.random(10, true, true);
+        log.info("Open Custom locations: " + addQuotes(randomLocation));
+        driver.get("https://petstore-kafka.swagger.io/?location=" + randomLocation);
+        Helpers.waitInSeconds(1);
+        return randomLocation;
+    }
+
+    public String addAPetToCurrentLocation() {
+        String newPetName = page.petNameInput.getAttribute("value");
+        log.info("Add a new pet: " + addQuotes(newPetName));
+        page.addPetBtn.click();
+        assertThat(page.pets.size(), equalTo(1));
+        wait.until(ExpectedConditions.textToBe(FIRST_ROW_IN_TABLE, newPetName + "\nAVAILABLE"));
+        return newPetName;
+    }
+
+}
