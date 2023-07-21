e35df41ca53b518ad777b33071c01848d5e63813
dinom <dinomc222@mail.ru>
2023-07-21 20:38:41 +0300

    Added extra tests

--- src/main/java/denis_grimaliuc/actions/BaseActions.java
+++ src/main/java/denis_grimaliuc/actions/BaseActions.java
@@ -1,4 +1,4 @@
-package denis_grimaliuc.junit.actions;
+package denis_grimaliuc.actions;
 
 import denis_grimaliuc.AdoptPage;
 import helpers.Helpers;
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static denis_grimaliuc.AdoptPage.FIRST_ROW_IN_TABLE;
+import static denis_grimaliuc.AdoptPage.*;
 import static helpers.Helpers.addQuotes;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -43,24 +43,46 @@ public class BaseActions {
         return randomLocation;
     }
 
+    public void verifyPetAdded(String petName, int petsCountBeforeAdding) {
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(ROWS, petsCountBeforeAdding));
+        wait.until(ExpectedConditions.textToBe(FIRST_ROW_IN_TABLE, petName));
+    }
+
+
     public String addAPetToCurrentLocation() {
         String newPetName = page.petNameInput.getAttribute("value");
         addAPetToCurrentLocation(newPetName);
         return newPetName;
     }
 
-    public void verifyPetAdded(String petName) {
-        assertThat(page.pets.size(), equalTo(1));
-        wait.until(ExpectedConditions.textToBe(FIRST_ROW_IN_TABLE, petName));
+
+    public void addAPetToCurrentLocation(int petNum) {
+        for (int i = 0; i < petNum; i++) {
+            String newPetName = RandomStringUtils.random(10, true, true);
+            addAPetToCurrentLocation(newPetName);
+        }
     }
 
+    public void adoptPets(int petNum) {
+        assertThat(page.pets.size(), Matchers.greaterThanOrEqualTo(petNum));
+        for (int i = 0; i < petNum; i++) {
+            page.pets.get(i).click();
+        }
+        page.adoptButton.click();
+    }
+
+    public void verifyAdoptIsCreated(int adoptCount) {
+        log.info("Verify Current section contains: " + adoptCount + " adoptions");
+        wait.until(ExpectedConditions.numberOfElementsToBe(ADOPT_ROWS, adoptCount));
+    }
 
     public void addAPetToCurrentLocation(String petName) {
         log.info("Add a new pet: " + addQuotes(petName));
+        int petsCountBeforeAdding = page.pets.size();
         String clearShortcut = Keys.chord(Keys.CONTROL, "a") + Keys.BACK_SPACE;
         page.petNameInput.sendKeys(clearShortcut + petName);
         page.addPetBtn.click();
-        verifyPetAdded(petName);
+        verifyPetAdded(petName, petsCountBeforeAdding);
     }
 
 }
--- src/test/java/denis_grimaliuc/junit/UIJunitTests.java
+++ src/test/java/denis_grimaliuc/junit/UIJunitTests.java
@@ -2,14 +2,17 @@ package denis_grimaliuc.junit;
 
 import denis_grimaliuc.AdoptPage;
 import denis_grimaliuc.UIStepDefinition;
-import denis_grimaliuc.junit.actions.BaseActions;
+import denis_grimaliuc.actions.BaseActions;
 import helpers.Helpers;
 import org.apache.commons.lang.RandomStringUtils;
 import org.apache.log4j.Logger;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -44,13 +47,13 @@ public class UIJunitTests {
         wait = new WebDriverWait(driver, 5);
         stepResults = new ArrayList<>();
         page = new AdoptPage(driver, wait);
-        //        Runtime.getRuntime().addShutdownHook(new Thread(() -> driver.quit()));
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> driver.quit()));
         actions.openRandomLocation();
     }
 
     @AfterEach
     public void after() {
-        driver.quit();
+//        driver.quit();
     }
 
     @Test
@@ -65,6 +68,61 @@ public class UIJunitTests {
     public void emptyNameTest() {
         String randomPetName = "";
         actions.addAPetToCurrentLocation(randomPetName);
+    }
+
+
+    /**
+     * Test: Pet name can be
+     * 1 symbol pet name test
+     * Space containing pet name test
+     * Punctuation symbols containing pet name test
+     */
+    @ParameterizedTest(name = "Test Pet name can Be \"{0}\"")
+    @ValueSource(strings = {"N", "John  Will  123", "!Q#!$%^&*()_"})
+    public void testPetNameCanBe(String name) {
+        actions.addAPetToCurrentLocation(name);
+    }
+
+
+    /**
+     * Duplicate name of pet (a pet with already existing name should be added without problems )
+     */
+    @Test
+    @DisplayName("Test two different pets can be added with the same Name")
+    public void testDuplicatePetName() {
+        String petName = "Pet Name";
+        actions.addAPetToCurrentLocation(petName);
+        actions.addAPetToCurrentLocation(petName);
+        assertThat(page.pets.size(), Matchers.equalTo(2));
+        assertThat(page.pets.get(0).getText(), Matchers.containsString(petName));
+        assertThat(page.pets.get(1).getText(), Matchers.containsString(petName));
+
+    }
+
+    /**
+     * 12.	Add a pet in Current location, see result in the Game section, status of pet is changed
+     */
+    @Test
+    @DisplayName("Test two different pets can be added with the same Name")
+    public void addAPetAndVerifyTheGameSection() {
+        int sumOfPets = 5;
+        actions.addAPetToCurrentLocation(sumOfPets);
+        String validationText = page.theGamePetsInInfo.getText();
+        log.info("Verify '" + validationText + "' matches pattern: \"Pets\\\\sin\\\\s\\\\w+:\\\\s\"");
+        assertThat(validationText, Matchers.matchesPattern("Pets\\sin\\s\\w+:\\s" + sumOfPets));
+    }
+
+    /**
+     * Add a pet in Current location, see result in the Game section, status of pet is changed
+     */
+    @Test
+    @DisplayName("Verify User can adopt a Pet")
+    public void verifyAdoptionCanBeCreated() {
+        int petsToAdopt = 2;
+        actions.addAPetToCurrentLocation(3);
+        actions.adoptPets(petsToAdopt);
+        actions.verifyAdoptIsCreated(1);
+
     }
 
     @Test
