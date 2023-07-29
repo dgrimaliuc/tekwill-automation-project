c640d8ee290c77295ff6e4350fa17136d60a21e9
Roman <roman.marco@gmail.com>
2023-07-29 23:04:28 +0300

    Junit Test

---
+++ src/test/java/Roman_Marcov/junit/JUnitParametrizedTest.java
@@ -0,0 +1,86 @@
+package Roman_Marcov.junit;
+
+import Roman_Marcov.junit.actions.ActionsPage;
+import org.apache.commons.lang.RandomStringUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import roman_marcov.AdoptPagePets;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import static helpers.Helpers.stepResults;
+
+public class JUnitParametrizedTest {
+    WebDriver driver = null;
+    WebDriverWait wait = null;
+    AdoptPagePets page = null;
+    ActionsPage actions = null;
+
+
+    @BeforeEach
+    public void before() {
+        var pathToChrome = "src/main/resources/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", pathToChrome);
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(5, TimeUnit.SECONDS);
+        actions = new ActionsPage(driver);
+        wait = new WebDriverWait(driver, 3);
+        page = new AdoptPagePets(driver, wait);
+        stepResults = new ArrayList<>();
+        actions.openRandomLocation();
+    }
+
+    @AfterEach
+    public void after() {
+        driver.quit();
+    }
+
+    /**
+     * 1 symbol pet name test
+     * Space containing pet name test
+     * Punctuation symbols containing pet name test
+     */
+    @ParameterizedTest(name = "Test name pet: \"{0}\"")
+    @ValueSource(strings = {"k", "Pet and NamePet", "~`!@#$%^&*()-_=+.,"})
+    public void testNamePetCanBe(String name) {
+        actions.addedPetCheck(name);
+    }
+
+    @Test
+    @DisplayName("1 symbol pet name test")
+    public void oneSymbolName() {
+        String newPet = RandomStringUtils.random(1, true, true);
+        actions.addPetWithName(newPet);
+        actions.addedPetCheck(newPet);
+    }
+
+    @Test
+    @DisplayName("Space containing pet name test")
+    public void nameWithSpace() {
+        String newPetName = RandomStringUtils.random(5, true, true);
+        actions.addPetWithName(newPetName + " " + newPetName);
+        actions.addedPetCheck(newPetName + " " + newPetName);
+    }
+
+    @Test
+    @DisplayName("Punctuation symbols containing pet name test")
+    public void nameWithSpecSymbols() {
+        String characters = "~`!@#$%^&*()-_=+.,";
+        String newPetName = RandomStringUtils.random(10, characters);
+        actions.addPetWithName(newPetName);
+        actions.addedPetCheck(newPetName);
+    }
+}
