cfee21a56477949789f84ec2e49aa1a39e390515
Denis Grimaliuc <94832020+dgrimaliuc-crunchyroll@users.noreply.github.com>
2023-08-16 20:36:10 +0300

    Added rest assured (#57)

--- src/main/java/denis_grimaliuc/data/enums/Status.java
+++ src/main/java/denis_grimaliuc/data/enums/Status.java
@@ -1,22 +1,10 @@
 package denis_grimaliuc.data.enums;
 
 public enum Status {
-    ADOPTED("ADOPTED"),
-    APPROVED("APPROVED"),
-    ONHOLD("ONHOLD"),
-    AVAILABLE("AVAILABLE"),
-    DENIED("DENIED");
-
-    //    public static final String APPROVED = ;
-    private final String status;
-
-    Status(String status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return status;
-    }
+    ADOPTED,
+    APPROVED,
+    ONHOLD,
+    AVAILABLE,
+    DENIED
 
 }
