d6498e2eff8aa18db198305959132ae205ce66ee
Mirela <boaghemirela@gmail.com>
2023-11-17 00:22:25 +0200

    Git Diff Task

---
+++ src/main/java/mirela_b/RunnerBranchComparing.java
@@ -0,0 +1,9 @@
+package mirela_b;
+
+public class RunnerBranchComparing {
+    public static void main(String[] args) {
+        BranchComparing compareBranch = new BranchComparing();
+        compareBranch.executeGitDiff("");
+    }
+}
+
