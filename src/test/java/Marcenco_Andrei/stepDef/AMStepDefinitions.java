0f26c7d5d176618a39f866a68ddbf9cefbfc229c
Andrew Marcenco <andrew4242005@yahoo.com>
2024-08-03 19:04:10 +0300

    Main

---
+++ src/test/java/Marcenco_Andrei/stepDef/AMStepDefinitions.java
@@ -0,0 +1,14 @@
+package Marcenco_Andrei;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class AMStepDefinitions {
+    @Given("I have a string {string}")
+    public void iHaveAString(String arg0) {
+    }
+
+    @Then("I should see {string}")
+    public void iShouldSee(String arg0) {
+    }
+}
