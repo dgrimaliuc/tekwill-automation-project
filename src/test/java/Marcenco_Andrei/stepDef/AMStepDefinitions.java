f24b896464b32a9b4783b4fbb3bb549b457e8cf7
Andrew Marcenco <andrew4242005@yahoo.com>
2024-08-06 19:40:21 +0300

    origin master

--- src/test/java/Marcenco_Andrei/stepDef/AMStepDefinitions.java
+++ src/test/java/Marcenco_Andrei/stepDef/AMStepDefinitions.java
@@ -1,14 +1,3 @@
-package Marcenco_Andrei;
-
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-
+package Marcenco_Andrei.stepDef;
 public class AMStepDefinitions {
-    @Given("I have a string {string}")
-    public void iHaveAString(String arg0) {
-    }
-
-    @Then("I should see {string}")
-    public void iShouldSee(String arg0) {
-    }
 }
--- src/test/java/denis_grimaliuc/step_definitions/UIStepDefinition.java
+++ src/test/java/denis_grimaliuc/step_definitions/UIStepDefinition.java
@@ -78,4 +78,5 @@ public class UIStepDefinition {
             assertThat(actual, Matchers.lessThanOrEqualTo(expectedPrice));
         }
     }
+
 }
