2cf67599f288e9a26e473d082991fcb7bed88dfe
Chiril Bortnicov <td1900000@gmail.com>
2024-08-06 11:43:31 +0300

    HW

---
+++ src/test/java/chiril_bortnicov/stepDefinition/UIStepDefinition.java
@@ -0,0 +1,53 @@
+package chiril_bortnicov.stepDefinition;
+
+import io.cucumber.core.logging.Logger;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+public class UIStepDefinition {
+
+    WebDriver driver = null;
+    WebDriverWait wait = null;
+   // Logger log = Logger.getLogger(UIStepDefinition.class);
+    HashMap<String, Object> stepResults = null;
+
+
+    @Before
+    public void before() {
+        var pathToChrome = "src/main/resources/chromedriver-127.exe";
+        System.setProperty("webdriver.chrome.driver", pathToChrome);
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+//        wait = new WebDriverWait(driver, 10);
+//        actions = new BaseActions(driver);
+        stepResults = new HashMap<>();
+    }
+
+    @After
+    public void after() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @When("I Enter to the Page")
+    public void iEnterToThePage() {
+        driver.get("https://shopify-eta-drab.vercel.app/");
+    }
+
+    @Given("I check if the title page is named correctly")
+    public void iCheckIfTheTitlePageIsNamedCorrectly() {
+        driver.getTitle();
+    }
+}
---
+++ src/test/resources/chiril_bortnicov/FirstUITest.feature
@@ -0,0 +1,9 @@
+Feature: First UI Test
+
+  Scenario: Open Shopify Page
+    When I Enter to the Page
+
+
+  Scenario: Verify Page Title
+    When I Enter to the Page
+    Given I check if the title page is named correctly
