478c6fc315943c0baa9594e004f74b7b314338e7
vmurashev365 <66040666+vmurashev365@users.noreply.github.com>
2024-08-12 14:50:55 +0300

    VictorMurashev-homework-20240812 (#88)
    * VictorMurashev-homework-20240812
    
    * VictorMurashev-homework-20240812

--- src/test/java/victor_murashev/step_definitions/UIStepDefinition.java
+++ src/test/java/victor_murashev/step_definitions/UIStepDefinition.java
@@ -7,9 +7,6 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.datatable.DataTable;
-import java.util.List;
-import java.util.Map;
 import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
@@ -18,9 +15,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import static denis_grimaliuc.ui.shopify.pages.Shopify.*;
@@ -197,8 +192,6 @@ public class UIStepDefinition {
     }
 
 
-
-
     @When("Select size {string}")
     public void selectSize(String size) {
         WebElement filter = driver.findElement(getSizeByName(size));
@@ -266,5 +259,164 @@ public class UIStepDefinition {
         WebElement cardContainer = driver.findElement(shopifyPage.cardContainer);
         String containerText = cardContainer.getText();
         assertThat(containerText, equalTo(message));
+    }
+
+    //        driver.switchTo().alert()
+    @When("Select sorting {string}")
+    public void selectSorting(String sortingType) {
+        WebElement sortButton = driver.findElement(shopifyPage.sortingButton);
+        sortButton.click();
+        WebElement option = driver.findElement(getSortingOption(sortingType));
+        option.click();
+    }
+
+    @When("I sort items by price")
+    public void iSortItemsByPrice() {
+        var prices = driver.findElements(shopifyPage.cardPrices);
+        List<Integer> sortedPrices = new ArrayList<>();
+        for (WebElement price : prices) {
+            String stringPrice = price.getText().substring(1);
+            Integer intPrice = Integer.parseInt(stringPrice);
+            sortedPrices.add(intPrice);
+        }
+        Collections.sort(sortedPrices);
+
+        stepResults.put("sortedPrices", sortedPrices);
+    }
+
+    @Then("Verify sorting is {string}")
+    public void verifySortingIs(String sortingType) {
+        List<Integer> sortedPrices = (List<Integer>) stepResults.get("sortedPrices");
+
+        if (sortingType.equals("Ascending")) {
+//            List<Integer> copy = new ArrayList<>(sortedPrices);
+//            Collections.sort(copy);
+//            assertThat(sortedPrices, equalTo(copy));
+            for (int i = 0; i < sortedPrices.size() - 1; i++) {
+                Integer expectedPrice = sortedPrices.get(i);
+                Integer actualPrice = Integer.parseInt(driver.findElements(shopifyPage.cardPrices).get(i).getText().substring(1));
+                assertThat(actualPrice, equalTo(expectedPrice));
+            }
+        }
+    }
+
+
+    @When("Collect first item details")
+    public void collectFirstItemDetails() {
+        WebElement firstItem = driver.findElement(shopifyPage.firstItem);
+        String title = firstItem.findElement(By.id("card-title")).getText();
+        String price = firstItem.findElement(By.id("card-price")).getText();
+        String color = firstItem.findElement(shopifyPage.cardColors).getAttribute("data-t");
+        String size = firstItem.findElement(By.id("card-size")).getText();
+        String image = firstItem.findElement(By.id("card-image")).getAttribute("src");
+
+
+        Map<String, String> itemDetails = new HashMap<>();
+        itemDetails.put("title", title);
+        itemDetails.put("price", price);
+        itemDetails.put("color", color);
+        itemDetails.put("size", size);
+        itemDetails.put("image", image);
+
+
+        stepResults.put("item_details", itemDetails);
+
+
+    }
+
+    @When("Add to cart first item")
+    public void addToCartFirstItem() {
+        WebElement button = driver.findElement(shopifyPage.addToCart);
+        button.click();
+        actions.waitUntilPageToLoad();
+
+    }
+
+    @Then("Verify cart is not empty")
+    public void verifyCartIsNotEmpty() {
+        Map<String, String> itemDetails = (Map<String, String>) stepResults.get("item_details");
+
+        var cartItems = driver.findElements(shopifyPage.cartItems);
+        assertThat(cartItems.size(), equalTo(1));
+
+        WebElement cartItem = cartItems.get(0);
+        String title = cartItem.findElement(By.tagName("h4")).getText();
+        String price = cartItem.findElement(shopifyPage.itemPrice).getText();
+        String color = cartItem.findElement(By.xpath("//p[contains(text(),'Color')]")).getText();
+        String size = cartItem.findElement(By.xpath("//p[contains(text(),'Size')]")).getText();
+        String image = cartItem.findElement(By.tagName("img")).getAttribute("src");
+        String total = driver.findElement(shopifyPage.totalPrice).getText();
+
+        assertThat(title, equalTo(itemDetails.get("title")));
+        assertThat(price, equalTo("Price: " + itemDetails.get("price")));
+        assertThat(color, equalTo("Color: " + itemDetails.get("color")));
+        assertThat(size, equalTo(itemDetails.get("size")));
+        assertThat(image, equalTo(itemDetails.get("image")));
+        assertThat(total, equalTo("Total: " + itemDetails.get("price")));
+
+
+    }
+
+    @When("Open cart")
+    public void openCart() {
+        WebElement cartButton = driver.findElement(shopifyPage.cartButton);
+        cartButton.click();
+    }
+
+    @Then("Verify cart is empty")
+    public void verifyCartIsEmpty() {
+        WebElement emptyCart = driver.findElement(shopifyPage.emptyCardTitle);
+        assertThat(emptyCart.getText(), equalTo("Your cart is empty"));
+        delay(1000);
+    }
+
+    @When("User click on Plus button")
+    public void userClickOnPlusButton() {
+        WebElement plusButton = driver.findElement(shopifyPage.plusButton);
+        plusButton.click();
+    }
+
+    @When("User click on Plus button {int} times")
+    public void userClickOnPlusButtonTimes(Integer times) {
+        WebElement plusButton = driver.findElement(shopifyPage.plusButton);
+        for (int i = 0; i < times; i++) {
+            plusButton.click();
+            delay(1000);
+        }
+    }
+
+    @Then("Verify item quantity is {string}")
+    public void verifyItemQuantityIs(String quantity) {
+        WebElement quantityField = driver.findElement(shopifyPage.itemQuantity);
+        assertThat(quantityField.getText(), equalTo(quantity));
+    }
+
+    @Then("Verify price was increased {int} times")
+    public void verifyPriceWasIncreasedTimes(int quantity) {
+        Map<String, String> itemDetails = (Map<String, String>) stepResults.get("item_details");
+        String stringPrice = itemDetails.get("price").substring(1);
+        int price = Integer.parseInt(stringPrice);
+        int total = price * quantity;
+
+        String itemActualPrice = driver.findElement(shopifyPage.itemPrice).getText();
+        String actualTotalPrice = driver.findElement(shopifyPage.totalPrice).getText();
+
+        assertThat(itemActualPrice, equalTo("Price: $" + total));
+        assertThat(actualTotalPrice, equalTo("Total: $" + total));
+    }
+
+    @When("User click on Minus button")
+    public void userClickOnMinusButton() {
+        WebElement minusButton = driver.findElement(shopifyPage.minusButton);
+        minusButton.click();
+    }
+
+    @When("User click on Minus button {int} times")
+    public void userClickOnMinusButtonTimes(Integer times) {
+        WebElement minusButton = driver.findElement(shopifyPage.minusButton);
+        for (int i = 0; i < times; i++) {
+            minusButton.click();
+            delay(1000);
+        }
     }
 }
