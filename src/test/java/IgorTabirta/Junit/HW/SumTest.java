0d4dd3791c5a65b05c238c6f00010b66b45804c4
ulapidu <ulapidu@gmail.com>
2024-08-16 17:37:29 +0300

    Shopify tests changing

---
+++ src/test/java/IgorTabirta/Junit/HW/SumTest.java
@@ -0,0 +1,13 @@
+package IgorTabirta.Junit.HW;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SumTest {
+    @Test
+    public void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result, "2 + 3 should equal 5");
+    }
+}
