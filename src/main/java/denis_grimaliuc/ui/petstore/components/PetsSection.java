f67ee99f30343e15a02768c8fb357a56558e0233
dgrimaliuc <116957079+dgrimaliuc@users.noreply.github.com>
2024-08-19 20:28:09 +0300

    Petstore tests added (#114)
    Co-authored-by: denis.grimaliuc <denis.grimaliuc@crunchyroll.com>

---
+++ src/main/java/denis_grimaliuc/ui/petstore/components/PetsSection.java
@@ -0,0 +1,14 @@
+package denis_grimaliuc.ui.petstore.components;
+
+import helpers.customElements.Component;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class PetsSection extends Component {
+    @FindBy(tagName = "h2")
+    public WebElement title;
+
+    public PetsSection(WebElement parent) {
+        super(parent);
+    }
+}
