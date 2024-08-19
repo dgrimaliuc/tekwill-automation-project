975cc4e285ed64237efb74147088289ca38c3fe9
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2024-08-19 19:54:05 +0300

    Last shopify tests added

--- src/main/java/denis_grimaliuc/ui/shopify/pages/ShopifyV2.java
+++ src/main/java/denis_grimaliuc/ui/shopify/pages/ShopifyV2.java
@@ -1,11 +1,13 @@
 package denis_grimaliuc.ui.shopify.pages;
 
 import denis_grimaliuc.ui.shopify.components.Card;
+import denis_grimaliuc.ui.shopify.components.CartSection;
 import denis_grimaliuc.ui.shopify.components.GenderFilterSection;
 import denis_grimaliuc.ui.shopify.components.PriceFilterSection;
 import helpers.BasePage;
 import helpers.customElements.Components;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class ShopifyV2 extends BasePage {
@@ -15,8 +17,15 @@ public class ShopifyV2 extends BasePage {
 
     @FindBy(css = "[data-t=gender-section]")
     public GenderFilterSection genderFilters;
+
     @FindBy(css = ".card")
     public Components<Card> cards;
+
+    @FindBy(css = ".cart-button")
+    public WebElement cartIcon;
+
+    @FindBy(css = ".cart-cards-wrapper")
+    public CartSection cartSection;
 
     public ShopifyV2(WebDriver driver) {
         super(driver);
