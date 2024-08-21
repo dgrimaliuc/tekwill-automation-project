7b70584fdde5f2a4b7d7b6b6d45aac6bd13cec62
td1900000 <32271808+td1900000@users.noreply.github.com>
2024-08-21 18:32:24 +0300

    SomeChanges (#115)

---
+++ src/test/java/chiril_bortnicov/junit/petstore/LocationTests.java
@@ -0,0 +1,33 @@
+package chiril_bortnicov.junit.petstore;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+public class LocationTests extends BaseLocationTest {
+
+    @Test
+    @DisplayName("Change location test")
+    public void changeLocationTest() {
+        var expectedLocation = "New York";
+        petStore.location.locationInput.clear();
+        petStore.location.locationInput.sendKeys(expectedLocation);
+        petStore.location.changeLocationBtn.click();
+        var url = driver.getCurrentUrl();
+
+        assertThat(url, containsString("?location=" + expectedLocation.replaceAll(" ", "+")));
+
+        var expectedTitleP = "Pets in: " + expectedLocation;
+
+        assertThat(petStore.petSection.title.getText(), equalTo(expectedTitleP));
+
+        var expectedTitleA = "Adoptions in: " + expectedLocation;
+
+        assertThat(petStore.adoptionSection.title.getText(), equalTo(expectedTitleA));
+
+    }
+}
