927032ee273115e6368e593afb2ebc72b428ff84
Serghey <serghey@MacBook-Pro-von-Carolina.local>
2024-08-25 21:37:48 +0200

    Add Pest/Adoption classes. Tests to validate Infor section)

--- src/main/java/serghei_rubailo/ui/pet_store/components/PetsSection.java
+++ src/main/java/serghei_rubailo/ui/pet_store/components/PetsSection.java
@@ -4,6 +4,8 @@ import helpers.customElements.Component;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class PetsSection extends Component {
     public PetsSection(WebElement parent) {
         super(parent);
@@ -11,5 +13,22 @@ public class PetsSection extends Component {
 
     @FindBy(tagName = "h2")
     public WebElement header;
+
+    @FindBy(css = "[data-t=single-pet]")
+    public List<WebElement> pets;
+
+//    public WebElement petsLocator
+
+    @FindBy(css = "[data-t=add-pet-button]")
+    public WebElement addPetButton;
+
+    @FindBy(css = "[data-t=adopt-button]")
+    public WebElement adoptButton;
+
+    public void addPets(int quantity) {
+        for (int i = 0; i < 5; i++) {
+            addPetButton.click();
+        }
+    }
 
 }
