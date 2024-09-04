b670cfa0c719a24ef48fe8ba85de11b0e30d9bb2
Serghei R <61268573+serghei-rubailo@users.noreply.github.com>
2024-09-04 17:27:53 +0200

    SergheiRubailo - JUnit petstore tests (#140)
    * Class work 19082024
    
    * Complete Location test
    
    * Add Pest/Adoption classes. Tests to validate Infor section)
    
    * Reveret unnecessary changes
    
    * SR-CW-28082024
    
    * SR-CW-28082024
    
    * complete class work
    
    * Add Adopt button hover test
    
    * Class work 19082024
    
    * Complete Location test
    
    * Add Pest/Adoption classes. Tests to validate Infor section)
    
    * SR-CW-28082024
    
    * SR-CW-28082024
    
    * complete class work
    
    * Add Adopt button hover test
    
    ---------
    
    Co-authored-by: Serghey <serghey@MacBook-Pro-von-Carolina.local>
    Co-authored-by: serghei-rubailo <serghei.rubauilo@gmail.com>

---
+++ src/test/java/serghei_rubailo/step_definition/junit/pet_store/BaseLocationTest.java
@@ -0,0 +1,17 @@
+package serghei_rubailo.step_definition.junit.pet_store;
+
+import internal.BaseTest;
+import org.junit.jupiter.api.BeforeEach;
+import serghei_rubailo.ui.pet_store.pages.PetStore;
+
+public class BaseLocationTest extends BaseTest {
+
+    PetStore petStore = new PetStore(driver);
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        petStore.openPage();
+    }
+
+}
