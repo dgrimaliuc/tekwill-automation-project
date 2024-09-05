898c14bad27985fd622252d9f079c33acca9dde8
Chiril Bortnicov <td1900000@gmail.com>
2024-09-05 18:22:32 +0300

    Lesson04.09+HWChirilB.

---
+++ src/main/java/chiril_bortnicov/data/enums/Status.java
@@ -0,0 +1,15 @@
+package chiril_bortnicov.data.enums;
+
+public enum Status {
+    ADOPTED,
+    APPROVED,
+    ONHOLD,
+    AVAILABLE,
+    DENIED;
+
+    @Override
+    public String toString() {
+        return name().toLowerCase();
+    }
+
+}
