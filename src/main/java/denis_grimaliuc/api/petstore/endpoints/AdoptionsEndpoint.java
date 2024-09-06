a1b06b034400afe664670016c6ca8826b4978bdf
dgrimaliuc <116957079+dgrimaliuc@users.noreply.github.com>
2024-09-06 19:48:21 +0300

    Created adoption test (#150)
    * Fixes
    
    
    ---------
    
    Co-authored-by: denis.grimaliuc <denis.grimaliuc@crunchyroll.com>

--- src/main/java/denis_grimaliuc/api/petstore/endpoints/AdoptionsEndpoint.java
+++ src/main/java/denis_grimaliuc/api/petstore/endpoints/AdoptionsEndpoint.java
@@ -1,7 +1,10 @@
 package denis_grimaliuc.api.petstore.endpoints;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import denis_grimaliuc.api.petstore.PetstoreBaseRequest;
 import io.restassured.response.Response;
+
+import java.util.List;
 
 public class AdoptionsEndpoint extends PetstoreBaseRequest {
 
@@ -16,10 +19,40 @@ public class AdoptionsEndpoint extends PetstoreBaseRequest {
             request.queryParam("status", status);
         }
 
-        var response = request
-                .when()
-                .get("/adoptions");
+        var response = request.when().get("/adoptions");
 
         return handleResponse(response);
     }
+
+    public static Response createAdoption(String location) {
+        return createAdoption(location, List.of());
+
+    }
+
+    public static Response createAdoption() {
+        return createAdoption("", List.of());
+    }
+
+    public static Response createAdoption(String location, List<String> pets) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonList;
+        try {
+            // Convert list to JSON
+            jsonList = objectMapper.writeValueAsString(pets);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+        var response = given().body("""
+                {
+                "location": "%s",
+                "pets": %s
+                }
+                """.formatted(location, jsonList)).when().post("/adoptions");
+
+        return handleResponse(response);
+    }
+
 }
