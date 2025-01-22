0a42ad00cc7e7f0f58612f1335780d751f5725be
roliya <liliyarosca@gmail.com>
2025-01-22 17:27:54 +0200

    basics 15-17.01 without HW

--- src/test/java/Lilia_Rosca/LRStepdefinition.java
+++ src/test/java/Lilia_Rosca/LRStepdefinition.java
@@ -6,13 +6,15 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.ro.Si;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
 
 public class LRStepdefinition {
-Map<String, String> context = new HashMap<>();
+    Map<String, Object> context = new HashMap<>(); //pentru a trasminte parametru de la o etoda la alta
 
     @Given("I have a string {string}")
     public void iHaveAString(String string) {
@@ -21,38 +23,86 @@ Map<String, String> context = new HashMap<>();
 
     @When("I check if it contains {string}")
     public void iCheckIfItContains(String subString) {
-        String myString = context.get("my_string");
+        String myString = (String) context.get("my_string");
         boolean contains = myString.contains(subString);
-        context.put("contains", String.valueOf(contains));
+        context.put("contains", contains);
     }
 
     @Then("It should return true")
     public void itShouldReturnTrue() {
-        String contains = context.get("contains");
-        assertThat(contains, equalTo("true"));
+        Boolean contains = (Boolean) context.get("contains");
+        assertThat(contains, equalTo(true));
     }
 
-    // reverse a string - 2 methods - to correct
- /*   @When("I reverse the string")
-    public void iReverseTheString(String subString) {
-        String myString = context.get("my_string");
-        boolean contains = myString.contains(subString);
-        context.put("contains", String.valueOf(contains));
+    // reverse a string - 2 methods
+    @When("I reverse the string")
+    public void iReverseTheString() {
+        String myString = (String) context.get("my_string");
+        String reversedString = new StringBuilder(myString)
+                .reverse()
+                .toString();
+        context.put("reversedString", String.valueOf(reversedString));
     }
 
     @Then("The reversed string should be {string}")
-    public void theReversedStringShouldBe(String arg0) {
-    }*/
-
-    /*@When("I check the length of the string")
-    public void iCheckTheLengthOfTheString() {
-        String len = context.get("my_string");
-        int length = myString.length(); // de modificat map
-        context.put("length of string ", length);
+    public void theReversedStringShouldBe(String string) {
+        String reversedString = (String) context.get("reversedString");
+        assertThat(reversedString, equalTo(string));
     }
 
-    @Then("the length should be {string}")
-    public void theLengthShouldBe(String arg0) {
+    // length of a string
+    @When("I check the length of the string")
+    public void iCheckTheLengthOfTheString() {
+        String myString = (String) context.get("my_string");
+        int len = myString.length(); // de modificat map
+        context.put("length_of_string", len);
+    }
 
-    }*/
+    @Then("the length should be {int}")
+    public void theLengthShouldBe(Integer num) {
+        Integer actualLength = (Integer) context.get("length_of_string");
+        assertThat(actualLength, equalTo(num));
+    }
+
+    @Given("I have the following paragraph:")
+    public void i_have_the_following_paragraph(String docString) {
+        context.put("paragrath", docString);
+    }
+
+    @When("I count the number of words")
+    public void i_count_the_number_of_words() {
+        String paragrath = (String) context.get("paragrath");
+        int wordsCount = paragrath.split("(\\s|\\n)").length;
+        context.put("words_count", wordsCount);
+    }
+
+    @Then("the word count should be {int}")
+    public void the_word_count_should_be(Integer expectedWordsCount) {
+        int actualWordsCount = (int) context.get("words_count");
+        assertThat(actualWordsCount, equalTo(expectedWordsCount));
+    }
+
+    @Given("I have a list with elements")
+    public void iHaveAListWithElements(List<String> string) {
+        context.put("my_list", string);
+    }
+
+    /* in trei pasi
+        @When("I check if list contains {string}")
+        public void iCheckIfListContainsBanana(String fruit) {
+            List<String> list = (List<String>) context.get("my_list");
+            boolean contains = list.contains(fruit);
+            context.put("contains", contains);
+        }
+        @Then("list should contain banana")
+        public void listShouldContain() {
+            Boolean contains = (Boolean) context.get("contains");
+            assertThat(contains, equalTo(true));
+        }*/
+    @Then("list should contain {string}")
+    public void listShouldContain(String fruit) {
+        List<String> list = (List<String>) context.get("my_list");
+        assertThat(list, hasItem(fruit));
+    }
+
 }
--- src/test/resources/Lilia_Rosca/lr_first.feature
+++ src/test/resources/Lilia_Rosca/lr_first.feature
@@ -1,16 +1,37 @@
 Feature: My first test
-
-  Scenario: Validarea ca "Hello folks!" contine "Hello"   // Ex1
+# Ex 1
+  Scenario: Validarea ca "Hello folks!" contine "Hello"
     Given I have a string "Hello folks!"
     When I check if it contains "Hello"
     Then It should return true
-
-  Scenario: Reverse a string    // Ex 2
+# Ex 2
+  Scenario: Reverse a string
     Given I have a string "Hello"
     When I reverse the string
     Then The reversed string should be "olleH"
-
+# Ex 3
   Scenario: String length validation
     Given I have a string "Java Help"
     When I check the length of the string
     Then the length should be 9
+
+# Ex 4 multiline string
+  Scenario: Count words in a paragrath
+    Given I have the following paragraph:
+    """
+    This is a test paragrath. It has several sentences.
+    Each sentence has multiple words.
+    """
+    When I count the number of words
+    Then the word count should be 14
+
+# Ex 5 contains a string
+  Scenario: List contains banana
+    Given I have a list with elements
+    |Apple|
+    |Banana|
+    |Orange|
+    |Apple|
+ #   When I check if list contains "Banana"  - daca in 2 pasi
+ #   Then list should contain banana
+    Then list should contain "Banana"
