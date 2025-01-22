b0fbce30cc216e2dbe3d91e292348c335255746a
roliya <liliyarosca@gmail.com>
2025-01-22 18:40:49 +0200

    basics 15-17.01 without HW

--- src/test/java/Lilia_Rosca/LR_2025_01_15_17.java
+++ src/test/java/Lilia_Rosca/LR_2025_01_15_17.java
@@ -1,27 +1,22 @@
 package Lilia_Rosca;
 
 public class LR_2025_01_15_17 {
-/*    @When("I check if it contains {string}")
-    public void i_check_if_it_contains(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("It should return true")
-    public void it_should_return_true() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }*/
-
-
 // 17.01        BDD ++++
     // i have a multiline string, a list, a map, a list of map
+        // CoreMatcher - pentru a gasi metoda, daca nu tii minte
+    // Ex 1 - validarea
 
-    // CoreMatcher - pentru a gasi metoda, daca nu tii minte
+    // Ex 2 - reversarea textului
+
+    // Ex 3 - length - de sine statator
 
 //  20.01 Xpath
-// 1. book class="yellow" - //book[@class = "yellow"] // @ - class
-// 2. book with id - //book[@id]
-// 3. book with genre - //book[genre] // [] - element
-// 4. book price > 9  - //book[price > 9]
+    // 1. book class="yellow" - //book[@class = "yellow"] // @ - class
+    // 2. book with id - //book[@id]
+    // 3. book with genre - //book[genre] // [] - element
+    // 4. book price > 9  - //book[price > 9]
+
+// 22.01
+
+
 }
