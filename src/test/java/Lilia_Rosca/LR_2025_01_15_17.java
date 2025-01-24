8385d879693347ca8f0c55326728020d5cee0bf6
roliya <liliyarosca@gmail.com>
2025-01-24 17:46:02 +0200

    HW 17.01

--- src/test/java/Lilia_Rosca/LR_2025_01_15_17.java
+++ src/test/java/Lilia_Rosca/LR_2025_01_15_17.java
@@ -10,13 +10,20 @@ public class LR_2025_01_15_17 {
 
     // Ex 3 - length - de sine statator
 
-//  20.01 Xpath
-    // 1. book class="yellow" - //book[@class = "yellow"] // @ - class
+//  20.01 Xpath mai multa info link prezentarea slide 5
+    // 1. book class = "yellow" - //book[@class = "yellow"] // @ - class
+    // pot fi adaugate mai multe elemente prin - nd
     // 2. book with id - //book[@id]
     // 3. book with genre - //book[genre] // [] - element
     // 4. book price > 9  - //book[price > 9]
+    //   toate elementele din book - //book/*
 
-// 22.01
+// 22.01 Implicit/Explicit wait,
+    // ExpectedCondition - metode,
+    // POM - page object model in selenium, Java class pentru o pagina, must have in auto testing
+    //
+
+
 
 
 }
