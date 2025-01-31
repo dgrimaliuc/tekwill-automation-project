2e18655ecfe5695091b1007c6034ae7eb1879b42
roliya <liliyarosca@gmail.com>
2025-01-31 17:13:39 +0200

    29.01 class + HW

--- src/main/java/Lilia_Rosca/components/LR_shopify/LR_PriceSection.java
+++ src/main/java/Lilia_Rosca/components/LR_shopify/LR_PriceSection.java
@@ -5,14 +5,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class LR_PriceSection extends Component {
+// 29.01
+    @FindBy(css = "[value = 'Under $25']")
+    public WebElement under25;
 
-/*    @FindBy(value = "")
-    public WebElement under25;*/
-    /* under 25
-     25 - 50
-     50 - 100
-     other 100
-    */
+    @FindBy(css = "[value = '$25 to $50']")
+    public WebElement p25To50;
+
+    @FindBy(css = "[value = '$50 to $100']")
+    public WebElement p50To100;
+
+    @FindBy(css = "[value = 'Over $100']")
+    public WebElement over100;
+
     public LR_PriceSection(WebElement parent) {
         super(parent);
     }
