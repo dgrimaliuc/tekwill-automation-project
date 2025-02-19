a8205d5aabba36d6252ea37495b4332b18032b97
liliarosca <liliyarosca@gmail.com>
2025-02-19 16:30:02 +0200

    17.02 (#212)

--- src/test/java/Lilia_Rosca/LR_JUnit/NeonStream/LR_SearchTests.java
+++ src/test/java/Lilia_Rosca/LR_JUnit/NeonStream/LR_SearchTests.java
@@ -24,16 +24,13 @@ public class LR_SearchTests extends LR_NeonStreamBaseTest{
     public void searchForSomethingTest() {
         header.searchInput.click();
         header.searchInput.sendKeys("the");
-//        actions.waitForNumberOfElementsToBeMoreThan(searchPage.cards, 5); // 5, nici cu 1 nu merge - Timeout
-//      waiting for example.actions.BaseActions$$Lambda/0x0000023401289a08@6889c8ee
-//      (tried for 5 second(s) with 200 milliseconds interval) - (BaseActions.java:268)
-
-/*        for (var card : searchPage.cards.subList(0, 5)) {
-shouldHaveTextContains(card.title, "the", true);
-}
-var searchCardsSize = searchPage.cards.size();
-actions.scrollTo(footer.container);
-actions.waitForNumberOfElementsToBeMoreThan(searchPage.cards, searchCardsSize); */
+        actions.waitForNumberOfElementsToBeMoreThan(searchPage.cards, 5);
+        for (var card : searchPage.cards.subList(0, 5)) {
+            actions.shouldHaveTextContains(card.title, "the", true);
+        }
+        var searchCardsSize = searchPage.cards.size();
+        actions.scrollTo(footer.container);
+        actions.waitForNumberOfElementsToBeMoreThan(searchPage.cards, searchCardsSize);
     }
     //    Click on result card test
     @Test
@@ -41,10 +38,9 @@ actions.waitForNumberOfElementsToBeMoreThan(searchPage.cards, searchCardsSize); 
     public void clickOnResultCardTest() {
         header.searchInput.click();
         header.searchInput.sendKeys("The");
-        actions.waitForNumberOfElementsToBeMoreThan(searchPage.cards, 0); // 5 - same error as in previous test
+        actions.waitForNumberOfElementsToBeMoreThan(searchPage.cards, 5);
         searchPage.cards.getFirst().click();
-//        actions.waitUntilPageToLoad(); // nu ajuta
-//        actions.shouldBeDisplayed(moviePage.contentHeader);// same error as in previous test
+        actions.shouldBeDisplayed(moviePage.contentHeader);// same error as in previous test
     }
 
     @Test
@@ -61,8 +57,8 @@ actions.waitForNumberOfElementsToBeMoreThan(searchPage.cards, searchCardsSize); 
     @Test
     @DisplayName("Direct navigation to search page with query test")
     public void directNavigationSearchTest() {
-        driver.get("https://neon-stream.web.app/search?q=12345678");
-        actions.waitForNumberOfElementsToBeMoreThan(searchPage.cards, 0); // 5 - same error as in previous test
-        actions.shouldHaveAttribute(header.searchInput, "value", "12345678");
+        driver.get("https://neon-stream.web.app/search?q=er");
+        actions.waitForNumberOfElementsToBeMoreThan(searchPage.cards, 5);
+        actions.shouldHaveAttribute(header.searchInput, "value", "er");
     }
 }
