5f3d51e2b0aee7dfe6a95f43cef950125557b6f2
Ion <ion@debatezone.eu>
2025-02-20 20:24:58 +0200

    EGorin api

--- src/test/java/IonErm/junit/api/petstore/ApiPetstoreTest.java
+++ src/test/java/IonErm/junit/api/petstore/ApiPetstoreTest.java
@@ -3,87 +3,35 @@ package IonErm.junit.api.petstore;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static example.api.petstore.PetstoreBaseRequest.given;
-import static example.api.petstore.PetstoreBaseRequest.handleResponse;
+import static IonErm.api.petstore.PetEndpoint.*;
+import static IonErm.utility.DogNameGenerator.generateDogName;
 import static example.data.utils.MatcherUtils.matchesJsonSchemaFrom;
 import static org.hamcrest.Matchers.*;
 
 public class ApiPetstoreTest {
 
+    public static String name = generateDogName();
+    public static String location = "Chisinau";
+    public static String status = "available";
+
+    /* GET */
     @Test
     @DisplayName("Get pet by id")
     public void getPetById() {
-        var response = given()
-                .queryParam("location", "Chisinau")
-                .pathParams("id", "3a63fc13-dc03-4c01-be52-abc1b68e466f")
-                .get("/pets/{id}");
-        handleResponse(response);
-        response
+        String pet_id = createPet(name, location).jsonPath().get("id");
+        getPet(pet_id, location)
                 .then()
                 .assertThat()
                 .statusCode(200)
                 .time(lessThan(5000L))
-                .body(matchesJsonSchemaFrom("src\\main\\resources\\schemes\\addPetSchema.json"));
-    }
-
-    @Test
-    @DisplayName("Get pet by empty id  test")
-    public void getPetByEmptyIdNegativeTest() {
-        var response = given()
-                .get("/pets");
-        handleResponse(response);
-        response
-                .then()
-                .statusCode(400)
-                .assertThat()
-                .time(lessThan(5000L))
-                .body("error", equalTo("Pet ID is invalid"));
-    }
-
-    @Test
-    @DisplayName("Get pet by wrong id  test")
-    public void getPetByWrongIdNegativeTest() {
-        var response = given()
-                .queryParam("location", "Chisinau")
-                .get("/pets/3a63fc13-dc03-4c01-be52-abc1b68e466w");
-        handleResponse(response);
-        response
-                .then()
-                .statusCode(400)
-                .assertThat()
-                .time(lessThan(5000L))
-                .body("error", equalTo("Pet ID is invalid"));
-    }
-
-    @Test
-    @DisplayName("Get pet by unexciting id  test")
-    public void getPetByUnexcitingIdTest() {
-        String pet_id = "53c6fa1b-876a-4690-b76f-abd3f022de6b";
-        var response = given()
-                .queryParam("location", "Chisinau")
-                .pathParams("id", pet_id)
-                .get("/pets/{id}");
-        handleResponse(response);
-        response
-                .then()
-                .statusCode(404)
-                .assertThat()
-                .time(lessThan(5000L))
-                .body("error", equalTo("Pet 53c6fa1b-876a-4690-b76f-abd3f022de6b not found in Chisinau"));
+                .body(matchesJsonSchemaFrom("src/main/resources/schemes/addPetSchema.json"));
     }
 
     @Test
     @DisplayName("Get multiple pets test")
     public void getMultiplePetsTest() {
-        String location = "Chisinau";
-        String statusAvailable = "available";
-        String statusAdopted = "adopted";
-        var response = given()
-                .queryParam("location", location)
-                .queryParam("status", statusAdopted)
-                .get("/pets");
-        handleResponse(response);
-        response
+        String status = "adopted";
+        getPets(status, location)
                 .then()
                 .assertThat()
                 .statusCode(200)
@@ -91,21 +39,46 @@ public class ApiPetstoreTest {
                 .body(matchesJsonSchemaFrom("src/main/resources/schemes/getPetsSchema.json"))
                 .body("size()", greaterThan(0))
                 .body("location", everyItem(equalTo(location)))
-                .body("status", everyItem(equalTo(statusAdopted)));
+                .body("status", everyItem(equalTo(status)));
     }
 
+    @Test
+    @DisplayName("Get pet by empty id test")
+    public void getPetByEmptyIdNegativeTest() {
+        getPet("", "")
+                .then()
+                .assertThat()
+                .statusCode(400)
+                .time(lessThan(5000L))
+                .body("error", equalTo("Pet ID is invalid"));
+    }
+
+    @Test
+    @DisplayName("Get pet by wrong id test")
+    public void getPetByWrongIdNegativeTest() {
+        getPet("1ab195fc-ea13-4258-be91-b63c81b7883t", "Chisinau")
+                .then()
+                .assertThat()
+                .statusCode(400)
+                .time(lessThan(5000L))
+                .body("error", equalTo("Pet ID is invalid"));
+    }
+
+    @Test
+    @DisplayName("Get pet by unexciting id test")
+    public void getPetByUnexcitingIdTest() {
+        getPet("53c6fa1b-876a-4690-b76f-abd3f022de6b", location)
+                .then()
+                .assertThat()
+                .statusCode(404)
+                .time(lessThan(5000L))
+                .body("error", equalTo("Pet 53c6fa1b-876a-4690-b76f-abd3f022de6b not found in Chisinau"));
+    }
 
     @Test
     @DisplayName("Get all pets test")
     public void getAllPetsTest() {
-        String location = "Chisinau";
-        String status = "available";
-        var response = given()
-                .queryParam("location", location)
-                .queryParam("status", status)
-                .get("/pets");
-        handleResponse(response);
-        response
+        getPets(status, location)
                 .then()
                 .statusCode(200)
                 .time(lessThan(3000L))
@@ -116,35 +89,20 @@ public class ApiPetstoreTest {
     }
 
     @Test
-    @DisplayName("Get all pets negative test")
-    public void getAllPetsNegativeTest() {
-        var response = given()
-                .queryParam("q", "Q")
-                .get("/pets");
-        handleResponse(response);
-        response
+    @DisplayName("Get all pets empty location test")
+    public void getAllPetsEmptyLocationTest() {
+        getPets(status, "")
                 .then()
                 .statusCode(400)
                 .time(lessThan(3000L))
                 .body("error", equalTo("Missing location"));
-
     }
 
+    /* POST */
     @Test
     @DisplayName("Add pet positive test")
     public void addPetTest() {
-        String location = "Chisinau";
-        String name = "QQQQ2";
-        var response = given()
-                .body(String.format("""
-                        {
-                          "location":"%s",
-                          "name":"%s"
-                        }
-                        """, location, name))
-                .post("/pets");
-        handleResponse(response);
-        response
+        createPet(name, location)
                 .then()
                 .assertThat()
                 .statusCode(201)
@@ -157,13 +115,7 @@ public class ApiPetstoreTest {
     @Test
     @DisplayName("Add pet negative test")
     public void addPetNegativeTest() {
-        var response = given()
-                .body("""
-                        {}
-                        """)
-                .post("/pets");
-        handleResponse(response);
-        response
+        createPet("", "")
                 .then()
                 .assertThat()
                 .statusCode(400)
@@ -174,10 +126,7 @@ public class ApiPetstoreTest {
     @Test
     @DisplayName("Empty body test")
     public void emptyBodyTest() {
-        var response = given()
-                .post("/pets");
-        handleResponse(response);
-        response
+        createPet(null, null, true)
                 .then()
                 .assertThat()
                 .statusCode(400)
@@ -185,36 +134,20 @@ public class ApiPetstoreTest {
                 .body(equalTo("Unexpected end of JSON input"));
     }
 
+    /* PATCH */
     @Test
     @DisplayName("Patch a single pet test")
     public void patchAPetTest() {
-        String location = "Chisinau";
-        String name = "PoP";
-        String newName = "MNBV";
-        String newStatus = "adopted";
-        var postResponse = given()
-                .body(String.format("""
-                        {
-                          "location":"%s",
-                          "name":"%s"
-                        }
-                        """, location, name))
-                .post("/pets");
-        String pet_id = postResponse.jsonPath().get("id");
-
-        var patchResponse = given()
-                .pathParams("id", pet_id)
-                .body(String.format(
-                        """
-                                {
-                                   "location":"%s",
-                                   "status":"%s",
-                                   "name":"%s"
-                                }
-                                """, location, newStatus, newName))
-                .patch("/pets{id}");
-        handleResponse(patchResponse);
-        patchResponse
+        String newName = generateDogName();
+        var pet_id = createPet(name, location).jsonPath().get("id").toString();
+        patchPet(pet_id, String.format(
+                """
+                            {
+                               "location":"%s",
+                               "status":"%s",
+                               "name":"%s"
+                            }
+                        """, location, status, newName))
                 .then()
                 .assertThat()
                 .time(lessThan(5000L))
@@ -222,69 +155,111 @@ public class ApiPetstoreTest {
                 .body(matchesJsonSchemaFrom("src/main/resources/schemes/addPetSchema.json"))
                 .body("location", equalTo(location))
                 .body("name", equalTo(newName))
-                .body("status", equalTo(newStatus));
+                .body("status", equalTo(status));
     }
 
     @Test
     @DisplayName("Patch single pet test")
     public void patchSinglePetTestTest() {
-        String location = "Chisinau";
-        String name = "UOUO";
-        String newName = "WQWQWQW";
-        String newStatus = "available";
-        var postResponse = given()
-                .body(String.format(
-                        """
-                                {
-                                    "name":"%s",
-                                    "location":"%s"
-                                }
-                                """, name, location
-                ))
-                .post("/pets");
-        String pet_id = postResponse.jsonPath().get("id");
-        var patchResponse = given()
-                .pathParams("id", pet_id)
-                .body(String.format(
-                        """
-                                {
-                                    "name":"%s",
-                                    "location":"%s",
-                                    "status":"%s"
-                                }
-                                """, newName, location, newStatus
-                ))
-                .patch("/pets{id}");
-        String newPet_id = patchResponse.jsonPath().get("id");
-        handleResponse(patchResponse);
-        patchResponse
+        String newName = generateDogName();
+        var pet_id = createPet(name, location).jsonPath().get("id").toString();
+        patchPet(pet_id, String.format(
+                """
+                        {
+                            "name":"%s",
+                            "location":"%s",
+                            "status":"%s"
+                        }
+                        """, newName, location, status
+        ))
                 .then()
                 .assertThat()
                 .statusCode(200)
                 .time(lessThan(4000L))
                 .body(matchesJsonSchemaFrom("src/main/resources/schemes/addPetSchema.json"))
                 .body("name", equalTo(newName))
-                .body("id", equalTo(newPet_id))
                 .body("location", equalTo(location))
-                .body("status", equalTo(newStatus));
+                .body("status", equalTo(status));
     }
 
     @Test
-    @DisplayName("Delete single pet")
-    public void deleteSinglePet() {
+    @DisplayName("Patch pet empty id and location test")
+    public void patchEmptyIdAndLocationPetTestTest() {
+        patchPet(null, ("""
+                {}
+                """))
+                .then()
+                .assertThat()
+                .statusCode(400)
+                .time(lessThan(4000L))
+                .body("error", equalTo("Props are required: id,location"));
+    }
+
+    @Test
+    @DisplayName("Patch pet invalid id test")
+    public void patchInvalidIdPetTestTest() {
+        String pet_id = "61d36976-401b-46e6-846b-10ec5f6a0f76"; //invalid
+        patchPet(pet_id, String.format(
+                """
+                        {
+                            "name":"%s",
+                            "location":"%s",
+                            "status":"%s"
+                        }
+                        """, name, location, status
+        ))
+                .then()
+                .assertThat()
+                .statusCode(404)
+                .time(lessThan(4000L))
+                .body(equalTo("Pet not found"));
+    }
+
+    /* DELETE */
+    @Test
+    @DisplayName("Delete single pet test")
+    public void deleteSinglePetTest() {
         String pet_id = "54a144a4-1bbf-453a-8b67-d2182b36371c";
-        String location = "Chisinau";
-        var response = given()
-                .queryParam("location", location)
-                .pathParam("id", pet_id)
-                .delete("/pets{id}");
-        handleResponse(response);
-        response
+        deletePet(pet_id, location)
                 .then()
                 .assertThat()
                 .statusCode(200)
                 .time(lessThan(3000L))
                 .body("message", equalTo("Pet removed: " + pet_id));
+    }
 
+    @Test
+    @DisplayName("Delete one pet test")
+    public void deleteOnePetTest() {
+        String pet_id = createPet(name, "Chisinau").jsonPath().get("id");
+        deletePet(pet_id, location)
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .time(lessThan(3000L))
+                .body("message", equalTo("Pet removed: " + pet_id));
+    }
+
+    @Test
+    @DisplayName("Delete one pet with invalid id test")
+    public void deleteOnePetWithInvalidIdTest() {
+        String pet_id = "###";
+        deletePet(pet_id, location)
+                .then()
+                .assertThat()
+                .statusCode(400)
+                .time(lessThan(3000L))
+                .body("error", equalTo("Pet ID is invalid"));
+    }
+
+    @Test
+    @DisplayName("Delete all pets test")
+    public void deleteAllPetsTest() {
+        deletePets(location)
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .time(lessThan(3000L))
+                .body("message", equalTo("Removed all pets from " + location));
     }
 }
