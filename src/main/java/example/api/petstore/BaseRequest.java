500e157916254984f485cc64dbfdefacc287b4c4
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2025-02-24 19:16:39 +0200

    Fix

--- src/main/java/example/api/petstore/BaseRequest.java
+++ src/main/java/example/api/petstore/BaseRequest.java
@@ -1,6 +1,5 @@
 package example.api.petstore;
 
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
@@ -9,15 +8,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BaseRequest {
 
-    static {
-        RestAssured.baseURI = "https://pet-store.nodeapp.workers.dev/api";
-    }
 
     @SafeVarargs
     public static <T> void assertResponse(T resp, Matcher<T>... matchers) {
         assertThat(resp, Matchers.allOf(matchers));
     }
- 
+
 
     public static Response handleResponse(Response response) {
         System.out.println("--- Response: ");
