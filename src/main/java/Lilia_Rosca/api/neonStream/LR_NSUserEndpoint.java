3d01b9a13b92bc7ae504fc34919ac8fc3a8e2b40
roliya <liliyarosca@gmail.com>
2025-02-26 20:45:40 +0200

    24-26.02 part

--- src/main/java/Lilia_Rosca/api/neonStream/LR_NSUserEndpoint.java
+++ src/main/java/Lilia_Rosca/api/neonStream/LR_NSUserEndpoint.java
@@ -3,32 +3,30 @@ package Lilia_Rosca.api.neonStream;
 import example.api.petstore.NeonStreamBaseRequest;
 import io.restassured.response.Response;
 
-import static org.apache.logging.log4j.util.Base64Util.encode;
-
 public class LR_NSUserEndpoint extends NeonStreamBaseRequest {
-// 24.02
-    public static Response createUser (String email, String password) { // Denis
+    // 24-26.02
+
+    public static Response createUser(String email, String password) {
+        var request = given()
+                .body(String.format("""
+                        {
+                        "email":"%s",
+                        "password":"%s"
+                        }
+                        """, email, password));
+        var response = request.post("/user/create");
+        handleResponse(response);
+        return response;
+    }
+    /*public static Response createUser (String email, String password) { // Denis aceleasi variabile???
         var request = given()
                 .header("authorization", "Basic " + encode(email + ":" + password));
         var response = request.post("/user/create");
         handleResponse(response);
         return response;
-    }
+    }*/
 
-    public static Response createUser() { // Denis
-        var request = given()
-                .body("""
-                        {
-                        "email":"",
-                        "password":""
-                        }
-                        """);
-        var response = request.post("/user/create");
-        handleResponse(response);
-        return response;
-    }
-
-    public static Response login (String email, String password) {
+    public static Response loginUser (String email, String password) {
         var request = given()
                 .body(String.format("""
                         {
@@ -40,4 +38,21 @@ public class LR_NSUserEndpoint extends NeonStreamBaseRequest {
         handleResponse(response);
         return response;
     }
+
+    // method for log in with Authorization header with Base64-encoded email and password
+
+    public static Response deleteUser (String email, String password) {
+        var request = given()
+                .body(String.format("""
+                        {
+                        "email":"%s",
+                        "password":"%s"
+                        }
+                        """, email, password));
+        var response = request.delete("/user");
+        handleResponse(response);
+        return response;
+    }
+
+    // method for delete a user and all its content using valid Authorization header with Base64-encoded email and password
 }
--- src/test/java/Lilia_Rosca/LR_JUnit/api/neonStream/LR_neonStreamUsersTests.java
+++ src/test/java/Lilia_Rosca/LR_JUnit/api/neonStream/LR_neonStreamUsersTests.java
@@ -1,36 +1,176 @@
 package Lilia_Rosca.LR_JUnit.api.neonStream;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static Lilia_Rosca.api.neonStream.LR_NSUserEndpoint.createUser;
-import static org.apache.commons.io.IOUtils.length;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.lessThan;
+import static Lilia_Rosca.api.neonStream.LR_NSUserEndpoint.*;
+import static example.data.utils.MatcherUtils.matchesJsonSchemaFrom;
+import static org.hamcrest.Matchers.*;
 
 public class LR_neonStreamUsersTests {
-// 24.02
-    @Test //valid email and password (meeting all complexity requirements) providing them in body
+// 24-26.02
+    @Test // create a user valid email and password (meeting all complexity requirements) providing them in body
     @DisplayName("Create user using body test")
     public void createUserUsingBodyTest() {
-        ;
+        String email = "test_lr_111@gmail.com"; // (RandomStringUtils.randomAlphanumeric(7) + "@gmail.com");
+        String password = "LR&123456test";
+        createUser(email, password)
+                .then().assertThat()
+                .statusCode(200)
+                .time(lessThan(2000L))
+                .body("email", equalTo(email))
+                .body(matchesJsonSchemaFrom("src/main/resources/schemes/neonStream/createUserSchema.json"));
+               // at least a number, uppercase letter, lowercase letter, special character
+    }
+
+    @Test
+    @DisplayName("Create user with invalid email test")
+    public void createUserWithInvalidEmailTest() {
+        String email = "test_lr_11@ gmail.com"; // containing space
+        String password = "LR&123456test";
+        createUser(email, password)
+                .then().assertThat()
+                .statusCode(400)
+                .time(lessThan(2000L))
+                .body("error.message", equalTo("INVALID_EMAIL"));
+    }
+
+    @Test
+    @DisplayName("Create user with empty password test")
+    public void createUserWithEmptyPasswordTest() {
+        String email = "test_lr_111@gmail.com"; // containing space
+        String password = "";
+        createUser(email, password)
+                .then().assertThat()
+                .statusCode(400)
+                .time(lessThan(2000L))
+                .body("error", equalTo("Missing required fields: password"));
+    }
+
+    @Test // verify that all errors are returned in response
+    @DisplayName("Create user with wrong password test")
+    public void createUserWithWrongPasswordTest() {
+        String email = "test_lr_111@gmail.com";
+        String password = "111"; // only numbers, too short
+        createUser(email, password)
+                .then().assertThat()
+                .statusCode(400)
+                .time(lessThan(2000L))
+                .body("error.message", equalTo("PASSWORD_DOES_NOT_MEET_REQUIREMENTS : Missing password requirements: [Password must contain at least 6 characters, Password must contain a lower case character, Password must contain an upper case character, Password must contain a non-alphanumeric character]"));
+    }
+    @Test
+    @DisplayName("Create user with wrong password 2 test")
+    public void createUserWithWrongPassword2Test() {
+        String email = "test_lr_111@gmail.com";
+        String password = "Aaaaaaaaaaaaaaaaaaaaaa%aaa"; // no numbers, too long
+        createUser(email, password)
+                .then().assertThat()
+                .statusCode(400)
+                .time(lessThan(2000L))
+                .body("error.message", equalTo("PASSWORD_DOES_NOT_MEET_REQUIREMENTS : Missing password requirements: [Password may contain at most 25 characters, Password must contain a numeric character]"));
+    }
+
+    @Test
+    @DisplayName("Create user with same email test")
+    public void createUserWithSameEmailTest() {
+        String email = "test_lr_111@gmail.com";
+        String password = "LR&123456test";
+        createUser(email, password)
+                .then().assertThat()
+                .statusCode(400)
+                .time(lessThan(2000L))
+                .body("error.message", equalTo("EMAIL_EXISTS"));
     }
 
     @Test //using Authorization header with Base64-encoded email and password (meeting all complexity requirements)
     @DisplayName("Create user using Basic auth test")
     public void createUserUsingBasicAuthTest() {
-        String email = "test_lr_111@gamil.com";
-        String password = "LR&123456test";
-        int passwordLength = length(password);
-        createUser(email, password)
-                .then().assertThat()
-                .statusCode(200)
-                .time(lessThan(1000L))
-                .header(email,containsString("@"))
-         //       .header("password",);
-
-        ;
     }
 
+    // Fail to create a new user with Authorization header containing invalid Base64 encoding
+    //  atob ('string')
 
+    @Test
+    @DisplayName("Successfully Login Test")
+    public void successfullyLoginTest(){
+        String email = "test_lr_111@gmail.com";
+        String password = "LR&123456test";
+        loginUser(email, password)
+                .then().assertThat()
+                .statusCode(200)
+                .time(lessThan(2000L))
+                .body("email", equalTo(email))
+                // schema - to create ??
+                .body("registered", equalTo(true));
+    }
+    @Test
+    @DisplayName("Login with incorrect email Test")
+    public void loginWithIncorrectEmailTest(){
+        String email = "test_lr_11@ gmail.com"; // + space
+        String password = "LR&123456test";
+        loginUser(email, password)
+                .then().assertThat()
+                .statusCode(400)
+                .time(lessThan(2000L))
+                .body("error.message", equalTo("INVALID_EMAIL"));
+    }
+
+    @Test
+    @DisplayName("Login with incorrect password Test")
+    public void loginWithIncorrectPasswordTest(){
+        String email = "test_lr_111@gmail.com";
+        String password = "R&123456test";
+        loginUser(email, password)
+                .then().assertThat()
+                .statusCode(400)
+                .time(lessThan(2000L))
+                .body("error.message", equalTo("INVALID_LOGIN_CREDENTIALS"));
+    }
+
+    // Successfully log in using Authorization header with Base64-encoded email and password
+    // Fail to log in with incorrect encoded Basic token
+
+
+    @Test
+    @DisplayName("Delete User Test")
+    public void deleteUserTest(){
+        String email = "test_lr_222@gmail.com";
+        String password = "LR&123456test";
+        createUser(email, password);
+        deleteUser(email, password)
+                .then().assertThat()
+                .statusCode(200)
+                .time(lessThan(3000L))
+                .body("message", equalTo("User '" + email + "' deleted"));
+        loginUser(email, password) // is it necessary???
+                .then().assertThat()
+                .body("error.message", equalTo("INVALID_LOGIN_CREDENTIALS"));
+    }
+
+    @Test
+    @DisplayName("Delete Inexisting User Test")
+    public void deleteInexistingUserTest(){
+        String email = "test_lr_333@gmail.com";
+        String password = "LR&123456test";
+        deleteUser(email, password)
+                .then().assertThat()
+                .statusCode(400)
+                .time(lessThan(2000L))
+                .body("error.message", equalTo("INVALID_LOGIN_CREDENTIALS"));
+    }
+
+    @Test
+    @DisplayName("Delete already deleted User Test")
+    public void deleteAlreadyDeletedUserTest(){
+        String email = "test_lr_222@gmail.com";
+        String password = "LR&123456test";
+        deleteUser(email, password)
+                .then().assertThat()
+                .statusCode(400)
+                .time(lessThan(2000L))
+                .body("error.message", equalTo("INVALID_LOGIN_CREDENTIALS"));
+    }
+    // Successfully delete a user and all its content using valid Authorization header with Base64-encoded email and password
+    // Fail to delete a user if the request is missing authentication (both body and headers empty)
 }
