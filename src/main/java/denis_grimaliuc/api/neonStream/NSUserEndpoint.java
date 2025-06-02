63cfa320546143f11c32699a270c1c4459a5cd91
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2025-06-02 11:50:40 +0300

    Added auth

--- src/main/java/denis_grimaliuc/api/neonStream/NSUserEndpoint.java
+++ src/main/java/denis_grimaliuc/api/neonStream/NSUserEndpoint.java
@@ -3,14 +3,13 @@ package denis_grimaliuc.api.neonStream;
 import example.api.petstore.NeonStreamBaseRequest;
 import io.restassured.response.Response;
 
-import static org.apache.logging.log4j.util.Base64Util.encode;
-
 public class NSUserEndpoint extends NeonStreamBaseRequest {
 
     public static Response createUser(String email, String password) {
-        var request = given()
-                .header("authorization", "Basic " + encode(email + ":" + password));
-        var response = request.post("/user/create");
+        var response = given()
+                .auth().preemptive().basic(email, password)
+                .post("/user/create");
+
         handleResponse(response);
         return response;
     }
