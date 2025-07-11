634ea67cde2dc9c26859b9e5bcb4ad58a6763b85
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2025-07-11 20:27:02 +0300

    Added components

--- src/test/java/denis_grimaliuc/junit/ShopifyJunitTests.java
+++ src/test/java/denis_grimaliuc/junit/ShopifyJunitTests.java
@@ -1,36 +1,71 @@
 package denis_grimaliuc.junit;
 
+import denis_grimaliuc.shopify.Card;
+import denis_grimaliuc.shopify.CartItem;
 import denis_grimaliuc.shopify.ShopifyPage;
 import internal.BaseTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static denis_grimaliuc.shopify.ShopifyPage.formatPrice;
 
 public class ShopifyJunitTests extends BaseTest {
 
     ShopifyPage shopifyPage = new ShopifyPage(driver);
 
-    @Test
-    public void openShopifyPage() {
+    @BeforeEach
+    public void setup() {
         log.info("Opening the Shopify page");
         driver.get("https://shopify-eta-drab.vercel.app/#");
-        shopifyPage.addToCartButtons.getFirst().click();
+        actions.waitForNumberOfElementsToBeMoreThan(shopifyPage.cards, 0);
+    }
 
-        String title = shopifyPage.titles.getFirst().getText();
-        String color = shopifyPage.colors.getFirst().getAttribute("data-t");
-        String price = shopifyPage.prices.getFirst().getText();
-        String size = shopifyPage.sizes.getFirst().getText();
-        String image = shopifyPage.images.getFirst().getAttribute("src");
+    @Test
+    public void openShopifyPage() {
+        shopifyPage.cards.getFirst().addToCartButton.click();
+        Card card = shopifyPage.cards.getFirst();
+
+        String title = card.title.getText();
+        String color = card.color.getAttribute("data-t");
+        String price = card.price.getText();
+        String size = card.size.getText();
+        String image = card.image.getAttribute("src");
 
         shopifyPage.cartButton.click();
 
-        actions.shouldHaveAttribute(shopifyPage.cartItemImage, "src", image);
-        actions.shouldHaveTextToBe(shopifyPage.cartItemTitle, title);
-        actions.shouldHaveTextToBe(shopifyPage.cartItemPrice, price);
-        actions.shouldHaveTextToBe(shopifyPage.cartItemColor, color);
-        actions.shouldHaveTextToBe(shopifyPage.cartItemSize, size);
-        actions.shouldHaveTextToBe(shopifyPage.cartTotalPrice, price);
-        actions.shouldHaveTextToBe(shopifyPage.cartItemQuantity, "1");
+        CartItem cartItem = shopifyPage.cart.cartItems.getFirst();
+
+        actions.shouldHaveAttribute(cartItem.image, "src", image);
+        actions.shouldHaveTextToBe(cartItem.title, title);
+        actions.shouldHaveTextToBe(cartItem.price, price);
+        actions.shouldHaveTextToBe(cartItem.color, color);
+        actions.shouldHaveTextToBe(cartItem.size, size);
+        actions.shouldHaveTextToBe(cartItem.quantity, "1");
+        actions.shouldHaveTextToBe(shopifyPage.cart.totalPrice, price);
+    }
 
 
+    @Test
+    public void twoCartItemsTest() {
+        Integer fPrice = formatPrice(shopifyPage.cards.getFirst().price.getText());
+        shopifyPage.cards.getFirst().addToCartButton.click();
+
+        Integer sPrice = formatPrice(shopifyPage.cards.get(1).price.getText());
+        shopifyPage.cards.get(1).addToCartButton.click();
+
+        shopifyPage.cartButton.click();
+        actions.waitForNumberOfElements(shopifyPage.cart.cartItems, 2);
+
+        actions.shouldHaveTextToBe(shopifyPage.cart.totalPrice, "Total: $" + (fPrice + sPrice));
+    }
+
+    @Test
+    public void addOneItemTwiceTest() {
+        shopifyPage.cards.getFirst().addToCartButton.click();
+        shopifyPage.cards.getFirst().addToCartButton.click();
+        shopifyPage.cartButton.click();
+        actions.waitForNumberOfElements(shopifyPage.cart.cartItems, 1);
+        actions.shouldHaveTextToBe(shopifyPage.cart.cartItems.getFirst().quantity, "2");
     }
 
 }
