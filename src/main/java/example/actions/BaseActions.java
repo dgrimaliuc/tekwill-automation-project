92be6654383f37ec3d85f610ef3f16ce0f2555bb
Magda1213 <162966392+Magda1213@users.noreply.github.com>
2025-07-28 17:08:11 +0300

    MP's HW Neon Stream page

--- src/main/java/example/actions/BaseActions.java
+++ src/main/java/example/actions/BaseActions.java
@@ -21,6 +21,7 @@ public class BaseActions {
     private final WebDriver driver;
     private final WebDriverWait wait;
     public Object shouldBeDisplayed;
+    public WebElement searchInput;
 
     public BaseActions(WebDriver driver) {
         this.driver = driver;
@@ -205,15 +206,15 @@ public class BaseActions {
                 if (!arguments[0].getBoundingClientRect) {
                     return false
                 }
-                                
+                
                 const rect = arguments[0].getBoundingClientRect()
-                                
+                
                 const windowHeight = (window.innerHeight || document.documentElement.clientHeight)
                 const windowWidth = (window.innerWidth || document.documentElement.clientWidth)
-                                
+                
                 const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) > 0)
                 const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) > 0)
-                                
+                
                 return (vertInView && horInView)
                 """, element);
     }
