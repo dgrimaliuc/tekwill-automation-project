3809004ba3751bccfe952b9684886586f79931ee
LiliaToma <lilia.toma25@gmail.com>
2025-08-02 19:47:21 +0300

    Test site petstore, all sections

--- src/main/java/lilia_toma/Shopify/ShopifyPageLT.java
+++ src/main/java/lilia_toma/Shopify/ShopifyPageLT.java
@@ -1,36 +1,29 @@
 package lilia_toma.Shopify;
 
-import com.google.common.collect.ForwardingDeque;
-import example.components.shopify.PriceSection;
 import helpers.BasePage;
-import helpers.customElements.Component;
 import helpers.customElements.Components;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import javax.print.DocFlavor;
 import java.util.List;
-
-import static java.lang.Integer.parseInt;
-import static javax.management.openmbean.SimpleType.STRING;
 
 public class ShopifyPageLT extends BasePage {
     @FindBy(css = "[data-t='price-section']")
     public CardPrice cardPrice;
 
 
-//    @FindBy(xpath = "//input[@value='Under $25']")
-//    public WebElement under25PriceFilter;
-//
-//    @FindBy(xpath = "//input[@value='$25 to $50']")
-//    public WebElement price25to50Filter;
-//
-//    @FindBy(xpath = "//input[@value='$50 to $100']")
-//    public WebElement price50to100Filter;
-//
-//    @FindBy(xpath = "//input[@value='Over $100']")
-//    public WebElement over100PriceFilter;
+    @FindBy(xpath = "//input[@value='Under $25']")
+    public WebElement under25PriceFilter;
+
+    @FindBy(xpath = "//input[@value='$25 to $50']")
+    public WebElement price25to50Filter;
+
+    @FindBy(xpath = "//input[@value='$50 to $100']")
+    public WebElement price50to100Filter;
+
+    @FindBy(xpath = "//input[@value='Over $100']")
+    public WebElement over100PriceFilter;
 
     @FindBy(css = "[value*='Under $25']")
     public WebElement priceUnder25C;
